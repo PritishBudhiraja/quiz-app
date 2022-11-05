d260fa6591ed91a6cb5053043623a408733fccfb
Pritish Budhiraja <1805317@kiit.ac.in>
2022-11-06 00:36:46 +0530

    Custom Code Changes - Via Adding Packages.

---
+++ src/components/QuizStartPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function QuizStartPage() {
+  return (
+    <div style={{
+        fontFamily: "Nunito, sans-serif"
+    }}>QuizStartPage</div>
+  )
+}
+
+export default QuizStartPage
